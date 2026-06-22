cdee77bd41645ab79bf278d82125bed0fe9f381e
1989ks <162946448+1989ks@users.noreply.github.com>
2026-06-22 19:31:38 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE = 'cf-sim-v6';
+const FILES = ['./index.html'];
+self.addEventListener('install', e => e.waitUntil(
+  caches.open(CACHE).then(c => c.addAll(FILES))
+));
+self.addEventListener('fetch', e => e.respondWith(
+  caches.match(e.request).then(r => r || fetch(e.request))
+));
+self.addEventListener('activate', e => e.waitUntil(
+  caches.keys().then(keys => Promise.all(
+    keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+  ))
+));
